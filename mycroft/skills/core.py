fa4dfcd8154e0562e99d3f80fe92aad9d475c943
jarbasal <jarbasai@mailfence.com>
2018-10-03 15:32:03 +0100

    cleanup

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -1729,7 +1729,7 @@ class FallbackSkill(MycroftSkill):
     """
     fallback_handlers = {}
     skills_config = Configuration.get().get("skills", {})
-    override_order = skills_config.get("override", False)
+    override = skills_config.get("override", False)
     fallback_order = skills_config.get("fallback_order", [])
 
     def __init__(self, name=None, bus=None, use_settings=True):
@@ -1809,7 +1809,7 @@ class FallbackSkill(MycroftSkill):
 
         self.instance_fallback_handlers.append(wrapper)
 
-        if self.override_order:
+        if self.override:
             folder = self.root_dir.split("/")[-1]
             if folder in self.fallback_order:
                 priority = self.fallback_order.index(folder) + 1
